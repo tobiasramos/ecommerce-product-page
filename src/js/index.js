3f7fb5bd79811a60e3f2f9cee96dd82bf091fae4
Tobias <tobiasramossilva@gmail.com>
2023-02-16 18:04:17 -0300

    Fazendo a lógica para o carrossel ser de fato funcial, mas ainda falta alguns detalhes para ficar pronto

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,61 @@
+const slides = document.querySelectorAll(".slide")
+const carousel = document.getElementsByClassName(".carousel")
+
+let current_slide = 0
+
+
+function hideSlide() {
+    slides.forEach(item => item.classList.remove('on'))
+}
+
+function showSlide() {
+    slides[current_slide].classList.add('on')
+}
+
+function nextSlide() {
+    hideSlide()
+    if (current_slide == slides.length - 1) {
+        current_slide = 0
+    } else {
+        current_slide++
+    }
+    showSlide()
+}
+
+function backSlide() {
+    hideSlide()
+    if (current_slide == 0) {
+        current_slide = slides.length - 1
+    } else {
+        current_slide--
+    }
+    showSlide()
+}
+
+$('.arrowBack').click(function () {
+    backSlide()
+})
+
+$('.arrowNext').click(function () {
+    nextSlide()
+})
+
+
+
 function product1() {
-    document.getElementById('imgShowcase').src='src/images/image-product-1.jpg'
+    document.getElementById('imgShowcase').src = 'src/images/image-product-1.jpg'
 }
 
 function product2() {
-    document.getElementById('imgShowcase').src='src/images/image-product-2.jpg'
+    document.getElementById('imgShowcase').src = 'src/images/image-product-2.jpg'
 }
 
 function product3() {
-    document.getElementById('imgShowcase').src='src/images/image-product-3.jpg'
+    document.getElementById('imgShowcase').src = 'src/images/image-product-3.jpg'
 }
 
 function product4() {
-    document.getElementById('imgShowcase').src='src/images/image-product-4.jpg'
+    document.getElementById('imgShowcase').src = 'src/images/image-product-4.jpg'
 }
 
 
